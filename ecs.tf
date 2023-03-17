0a2cc28431c2297e8b749b0d48abc412801a1d84
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2023-03-17 16:53:22 +0530

    Changing capacity provider name

--- ecs.tf
+++ ecs.tf
@@ -1,10 +1,9 @@
 resource "aws_ecs_cluster" "ecs" {
 
-  count = 0
   depends_on = [aws_autoscaling_group.ecs, aws_ecs_capacity_provider.ecs_capacity_provider]
   name       = var.name
 
-  capacity_providers = ["${var.name}-capacity-provider"]
+  capacity_providers = ["${var.name}-new-capacity-provider"]
 
   lifecycle {
     ignore_changes = [
