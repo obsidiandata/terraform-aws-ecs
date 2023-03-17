d69b0b59fc263b17876a0cced49cca37b224e027
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2023-03-17 18:17:59 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -3,7 +3,7 @@ resource "aws_ecs_cluster" "ecs" {
   depends_on = [aws_autoscaling_group.ecs, aws_ecs_capacity_provider.ecs_capacity_provider]
   name       = var.name
 
-  capacity_providers = ["${var.name}-new-capacity-provider"]
+  capacity_providers = ["${var.name}-capacity-provider"]
 
   lifecycle {
     ignore_changes = [
